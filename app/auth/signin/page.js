16fe950b5df0933239215e9bbbf8b35dc525ea09
luotongzhou <luotongzhou1996@gmail.com>
2024-10-11 01:40:35 +0800

    feat: vercel deploy

--- app/auth/signin/page.js
+++ app/auth/signin/page.js
@@ -1,19 +1,30 @@
 'use client'
-export default async function SignIn() {
-  const response = await fetch('http://localhost:3000/api/auth/csrf');
-  const {csrfToken} = await response.json()
-  return (
-    <form method="post" action="/api/auth/callback/credentials">
-      <input type="hidden" name="csrfToken" value={csrfToken} />
-      <label>
-        Username
-        <input name="username" type="text" />
-      </label>
-      <label>
-        Password
-        <input name="password" type="password" />
-      </label>
-      <button type="submit">Sign in</button>
-    </form>
-  )
+
+import { useEffect, useState } from 'react'
+
+export default function SignIn() {
+	const [token, setToken] = useState('')
+
+	useEffect(() => {
+		async function fetchData() {
+			const response = await fetch(`//${location.host}/api/auth/csrf`)
+			const { csrfToken } = await response.json()
+			setToken(csrfToken)
+		}
+		fetchData()
+	}, [])
+	return (
+		<form method='post' action='/api/auth/callback/credentials'>
+			<input type='hidden' name='csrfToken' value={token} />
+			<label>
+				Username
+				<input name='username' type='text' />
+			</label>
+			<label>
+				Password
+				<input name='password' type='password' />
+			</label>
+			<button type='submit'>Sign in</button>
+		</form>
+	)
 }
